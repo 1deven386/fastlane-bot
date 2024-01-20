b929cc12348ff22c096d7db9900da7ae3ab4b414
sklbancor <109073706+sklbancor@users.noreply.github.com>
2024-01-20 11:02:20 +0000

    Functions -> package [v0.9.2]

--- fastlane_bot/tools/invariants/__init__.py
+++ fastlane_bot/tools/invariants/__init__.py
@@ -1,6 +1,18 @@
 """
 collection of tools for AMM invariant functions
 
+This library is independent of fastlane_bot; if you extract it, make sure
+you copy the following test notebooks as well.
+- NBTest_065_InvariantsDictVector
+- NBTest_066_InvariantsFunctions
+- NBTest_067_Invariants
+- NBTest_068_InvariantsAMM
+
+Corresponding Author: Stefan Loesch <stefan@bancor.network>
+Canonic Location: https://github.com/bancorprotocol/fastlane-bot
+
 (c) Copyright Bprotocol foundation 2024. 
 Licensed under MIT
 """
+__VERSION__ = '0.9+'
+__DATE__ = "20/Jan/2024+"
