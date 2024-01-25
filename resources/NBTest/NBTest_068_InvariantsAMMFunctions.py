e1fb3176be477d5601fc5a1230f69870fe633178
sklbancor <109073706+sklbancor@users.noreply.github.com>
2024-01-25 19:17:19 +0000

    NBTest
    68
    
    68

--- resources/NBTest/NBTest_068_InvariantsAMMFunctions.py
+++ resources/NBTest/NBTest_068_InvariantsAMMFunctions.py
@@ -36,16 +36,14 @@ print("{0.__name__} v{0.__VERSION__} ({0.__DATE__})".format(Kernel))
 # # AMM Functions  (Invariants Module; NBTest068)
 
 
-# ## Plots [NOTEST]
+# ## Constant product style AMMs [NOTEST]
 
 rg = rg0 = (1,20)
 xlim = (0,20)
 ylim = (0,10)
 p = lambda fn: str(f.fmt(fn.params(classname=True), ".2f"))
 
-# ### Constant-product-style AMMs
-
-# #### Plain constant product (Bancor V2.1, Bancor V3; Uniswap V2)
+# ### Plain constant product (Bancor V2.1, Bancor V3; Uniswap V2)
 #
 # $$
 # y(x) = \frac k x
@@ -74,7 +72,7 @@ plt.ylim(*ylim)
 plt.show()
 # -
 
-# #### Levered constant product (virtual token balances)
+# ### Levered constant product (virtual token balances)
 #
 # $$
 # y(x) + y_0 = \frac k {x+x_0}
@@ -101,7 +99,85 @@ plt.ylim(*ylim)
 plt.show()
 # -
 
-# #### Levered constant product (Uniswap V3)
+# #### `from_xpxp`
+#
+# alternative constructor, determining the curve by two points on a x-axis $x_a, x_b$ and the associated prices $p_a, p_b$; note that we are missing a parameter, $y_0$, which is a non-financial parameter in this case as a shift in the y direction does not affect prices as long as the curve does not run out of tokens
+#
+# We have the following equations:
+#
+# $$
+# \frac k {(x_0+x_a)^2} = p_a,\quad \frac k {(x_0+x_b)^2} = p_b
+# $$
+#
+#
+# Solving for $x_0, k$ we find
+#
+# $$
+# x_0 = \frac{-(p_a x_a) + \sqrt{p_a p_b (x_a - x_b)^2} + p_b x_b}{p_a - p_b}
+# $$
+#
+# $$
+# k = p_a \left(x_a + \frac{-(p_a x_a) + \sqrt{p_a p_b (x_a - x_b)^2} + p_b x_b}{p_a - p_b}\right)^2
+# = p_a (x_a + x_0)^2
+# $$
+#
+# or 
+#
+#     x0 = (-(pa * xa) + m.sqrt(pa * pb * (xa - xb)**2) + pb * xb) / (pa - pb)
+#     k  = pa * ((xa + (-(pa * xa) + m.sqrt(pa * pb * (xa - xb)**2) + pb * xb) / (pa - pb)) ** 2)
+#     k = pa * (xa + x0) ** 2
+#
+#
+
+assert raises(f.LCPMM.from_xpxp, xa=20, pa=2, xb=10, pb=1) == 'xa=20 must be < xb=10'
+assert raises(f.LCPMM.from_xpxp, xa=10, pa=2, xb=10, pb=1) == 'xa=10 must be < xb=10'
+assert raises(f.LCPMM.from_xpxp, xa=10, pa=1, xb=20, pb=2) == 'pa=1 must be > pb=2'
+assert raises(f.LCPMM.from_xpxp, xa=10, pa=1, xb=20, pb=1) == 'pa=1 must be > pb=1'
+assert raises(f.LCPMM.from_xpxp, 1,2,3,4) # kwargs!
+assert raises(f.LCPMM.from_xpxp, xa=10, pa=2, xb=20, pb=1, y0=1, ya=1, yb=1) == 'at most 1 of y0, ya, yb can be given, but got 3 [y0=1, ya=1, yb=1]'
+assert raises(f.LCPMM.from_xpxp, xa=10, pa=2, xb=20, pb=1, y0=1, ya=1)
+assert raises(f.LCPMM.from_xpxp, xa=10, pa=2, xb=20, pb=1, y0=1, yb=1)
+assert raises(f.LCPMM.from_xpxp, xa=10, pa=2, xb=20, pb=1, ya=1, yb=1)
+
+# +
+prm = dict(xa=10, pa=2, xb=20, pb=1)
+
+fn = f.LCPMM.from_xpxp(**prm)
+fn0 = fn
+assert iseq(fn.p(prm["xa"]), prm["pa"])
+assert iseq(fn.p(prm["xb"]), prm["pb"])
+assert fn.y0 == 0
+ya = fn(prm["xa"])
+yb = fn(prm["xb"])
+
+fn = f.LCPMM.from_xpxp(**prm, y0=10)
+assert fn.k == fn0.k
+assert fn.x0 == fn0.x0
+assert fn.y0 != fn0.y0
+assert iseq(fn.p(prm["xa"]), prm["pa"])
+assert iseq(fn.p(prm["xb"]), prm["pb"])
+assert fn.y0 == 10
+assert fn(prm["xa"]) == ya-10
+assert fn(prm["xb"]) == yb-10
+# -
+
+fn = f.LCPMM.from_xpxp(**prm, ya=100)
+assert fn.k == fn0.k
+assert fn.x0 == fn0.x0
+assert fn.y0 != fn0.y0
+assert iseq(fn.p(prm["xa"]), prm["pa"])
+assert iseq(fn.p(prm["xb"]), prm["pb"])
+assert fn(prm["xa"]) == 100
+
+fn = f.LCPMM.from_xpxp(**prm, yb=100)
+assert fn.k == fn0.k
+assert fn.x0 == fn0.x0
+assert fn.y0 != fn0.y0
+assert iseq(fn.p(prm["xa"]), prm["pa"])
+assert iseq(fn.p(prm["xb"]), prm["pb"])
+assert fn(prm["xb"]) == 100
+
+# ### Levered constant product (Uniswap V3)
 
 # +
 rg = (0,2)
@@ -109,6 +185,7 @@ assert f.UniV3 is f.UniV3Function
 fn1 = f.UniV3(Pa=4, Pb=2, L=5)
 fn2 = fn1.update(L=5)
 for fn in [fn1, fn2]:
+    print(fn)
     fn.plot(*rg, label=f"{p(fn)}")
 plt.title("Uniswap V3 -- Invariant")
 # plt.xlim(*xlim)
@@ -124,7 +201,7 @@ plt.ylabel("price (dy/dx)")
 plt.show()
 # -
 
-# #### Levered constant product (Carbon)
+# ### Levered constant product (Carbon)
 
 # +
 rg = (0,2)
@@ -147,9 +224,9 @@ plt.ylabel("price (dy/dx)")
 plt.show()
 # -
 
-# ### Other AMMs
+# ## Other AMMs [NOTEST]
 
-# #### Solidly
+# ### Solidly
 
 # +
 rg = (0.05,20)
